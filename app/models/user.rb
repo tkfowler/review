916994db430371cdd5cda56ba17c95de32bb920c
Thomas Fowler <tkfowler13@gmail.com>
2016-12-16 08:49:16 -0800

    second

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
   validates :first_name, :last_name, :email, :city, :state, presence: true
   validates :email, uniqueness: {case_sensitive: false}, format: { with: email_regex }
-  validates :password, presence:true, confirmation: true, on: :create
+  validates :password, presence:true, length: { minimum: 8 }, confirmation: true, on: :create
 end
